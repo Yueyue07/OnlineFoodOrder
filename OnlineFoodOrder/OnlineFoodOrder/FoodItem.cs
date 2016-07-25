687c4ff85f6edfc54d8a43288eee5084de8bb7f9
Yueyue Qin (InConsulting Inc.) <v-yueqin@microsoft.com>
2016-07-25 09:31:02 -0700

    setup FoodItem AND SiteAccount Class

---
+++ OnlineFoodOrder/OnlineFoodOrder/FoodItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineVegeFoodOrder
+{
+    public class FoodItem
+    {
+        #region Properties
+        /// <summary>
+        /// Food Item Id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Name of this food item
+        /// </summary>
+        public string FoodName { get; set; }
+        /// <summary>
+        /// Flavor Of this Food
+        /// </summary>
+        public string Flavor { get; set; }
+        /// <summary>
+        /// Ingredients of this food
+        /// </summary>
+        public string[] Ingredients { get; set; }
+        #endregion
+    }
+}
---
+++ OnlineFoodOrder/OnlineFoodOrder/SiteAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineVegeFoodOrder
+{
+    /// <summary>
+    /// This class defines Site Account 
+    /// </summary>
+    public class SiteAccount
+    {
+        #region Properties
+        /// <summary>
+        /// Account Id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Account Name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Account Email Address
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Real Address
+        /// </summary>
+        public string Address { get; set; }
+        #endregion
+
+        #region Methods
+        public void AddFoodItem(string foodName, string flavor, string[] ingredients)
+        {
+            var foodItem = new FoodItem { FoodName = foodName, Flavor = flavor, Ingredients = ingredients };
+
+
+        }
+        #endregion
+    }
+}
